917d0fb6a85db7763564e56bd01cd1345f235181
erpyjune <thanks.lot@gmail.com>
2016-07-22 09:48:52 +0900

    업데이트

---
+++ WindowsApplication1/StockValueInfo.vb
@@ -0,0 +1,42 @@
+﻿Public Class StockValueInfo
+    Private name As String
+    Private code As String
+    Private curDate As String
+    Private startV As Integer
+    Private endV As Integer
+    Private tv As Integer
+
+    Sub setStockValue(ByVal curDate As String, ByVal startVal As Integer, ByVal endVal As Integer, ByVal tradeVal As Integer, ByVal name As String, ByVal code As String)
+        Me.curDate = curDate
+        Me.startV = startVal
+        Me.endV = endVal
+        Me.tv = tradeVal
+        Me.name = name
+        Me.code = code
+    End Sub
+
+    Function getCurDate()
+        Return Me.curDate
+    End Function
+
+    Function getStartV()
+        Return Me.startV
+    End Function
+
+    Function getEndV()
+        Return Me.endV
+    End Function
+
+    Function getTradeV()
+        Return Me.tv
+    End Function
+
+    Function getName()
+        Return Me.name
+    End Function
+
+    Function getCode()
+        Return Me.code
+    End Function
+
+End Class
