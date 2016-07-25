73b5aa4c10dc78f9d9298a2e43b55e6038ac2dab
thanks.lot <thanks.lot@gmail.com>
2016-07-26 00:33:36 +0900

    update

---
+++ WindowsApplication1/StoreClass.vb
@@ -0,0 +1,20 @@
+﻿Public Class StoreClass
+    Dim listStockSaleBuyInfo As New List(Of StockSellBuyInfoSub)()
+
+    Sub addStockSaleBuyInfo(ByVal data As StockSellBuyInfoSub)
+        Me.listStockSaleBuyInfo.Add(data)
+    End Sub
+
+    Function getStockSaleBuyInfo()
+        Return Me.listStockSaleBuyInfo
+    End Function
+
+    Function getStockSaleBuyListCount()
+        Return Me.listStockSaleBuyInfo.Count
+    End Function
+
+    Sub setCleaerList()
+        Me.listStockSaleBuyInfo.Clear()
+    End Sub
+
+End Class
