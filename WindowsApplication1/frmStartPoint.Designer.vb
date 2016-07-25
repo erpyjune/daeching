1ab30601915d57d0658b26a5f007b79e8fea2d8e
erpyjune <thanks.lot@gmail.com>
2016-07-25 13:49:43 +0900

    update

--- WindowsApplication1/frmStartPoint.Designer.vb
+++ WindowsApplication1/frmStartPoint.Designer.vb
@@ -31,11 +31,11 @@ Partial Class frmStartPoint
         Me.주포1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.주포2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.주포3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.KHOpenAPI = New AxKHOpenAPILib.AxKHOpenAPI()
         Me.상장주식수 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.시가총액 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.영업이익 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.당기순이익 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.KHOpenAPI = New AxKHOpenAPILib.AxKHOpenAPI()
         CType(Me.KHOpenAPI, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -43,9 +43,9 @@ Partial Class frmStartPoint
         '
         Me.lstViewStartPoint.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.종목이름, Me.시작점날짜, Me.시작점유형, Me.종목상태, Me.주포1, Me.주포2, Me.주포3, Me.상장주식수, Me.시가총액, Me.영업이익, Me.당기순이익})
         Me.lstViewStartPoint.Font = New System.Drawing.Font("굴림", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
-        Me.lstViewStartPoint.Location = New System.Drawing.Point(9, 60)
+        Me.lstViewStartPoint.Location = New System.Drawing.Point(9, 25)
         Me.lstViewStartPoint.Name = "lstViewStartPoint"
-        Me.lstViewStartPoint.Size = New System.Drawing.Size(1022, 414)
+        Me.lstViewStartPoint.Size = New System.Drawing.Size(1022, 449)
         Me.lstViewStartPoint.TabIndex = 0
         Me.lstViewStartPoint.UseCompatibleStateImageBehavior = False
         Me.lstViewStartPoint.View = System.Windows.Forms.View.Details
@@ -85,15 +85,6 @@ Partial Class frmStartPoint
         Me.주포3.Text = "주포3"
         Me.주포3.Width = 66
         '
-        'KHOpenAPI
-        '
-        Me.KHOpenAPI.Enabled = True
-        Me.KHOpenAPI.Location = New System.Drawing.Point(686, 21)
-        Me.KHOpenAPI.Name = "KHOpenAPI"
-        Me.KHOpenAPI.OcxState = CType(resources.GetObject("KHOpenAPI.OcxState"), System.Windows.Forms.AxHost.State)
-        Me.KHOpenAPI.Size = New System.Drawing.Size(66, 20)
-        Me.KHOpenAPI.TabIndex = 1
-        '
         '상장주식수
         '
         Me.상장주식수.Text = "상장주식수"
@@ -111,6 +102,15 @@ Partial Class frmStartPoint
         '
         Me.당기순이익.Text = "당기순이익"
         Me.당기순이익.Width = 80
+        '
+        'KHOpenAPI
+        '
+        Me.KHOpenAPI.Enabled = True
+        Me.KHOpenAPI.Location = New System.Drawing.Point(965, -1)
+        Me.KHOpenAPI.Name = "KHOpenAPI"
+        Me.KHOpenAPI.OcxState = CType(resources.GetObject("KHOpenAPI.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.KHOpenAPI.Size = New System.Drawing.Size(66, 20)
+        Me.KHOpenAPI.TabIndex = 1
         '
         'frmStartPoint
         '
